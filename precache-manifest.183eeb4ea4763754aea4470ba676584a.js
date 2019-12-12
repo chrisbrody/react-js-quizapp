d18c60858d244b43c7b6857736ecb8dec3d558e7
Chris Brody <chrisbrody@codifyacademy.com>
2019-12-12 07:43:09 -0500

    Updates

---
+++ precache-manifest.183eeb4ea4763754aea4470ba676584a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e19b374213681b123a0d216f07d3f67",
+    "url": "/react-js-quizzapp/index.html"
+  },
+  {
+    "revision": "853b9f5dcbe6fbbb4007",
+    "url": "/react-js-quizzapp/static/css/main.ed8892c7.chunk.css"
+  },
+  {
+    "revision": "f411d4da7d0834162313",
+    "url": "/react-js-quizzapp/static/js/2.a40a013e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-js-quizzapp/static/js/2.a40a013e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "853b9f5dcbe6fbbb4007",
+    "url": "/react-js-quizzapp/static/js/main.b6443f03.chunk.js"
+  },
+  {
+    "revision": "e7c0666fecf2a6051ace",
+    "url": "/react-js-quizzapp/static/js/runtime-main.8077c40d.js"
+  }
+]);
